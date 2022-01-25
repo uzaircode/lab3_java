004c097395d881fba5b8870ea6e6903a9b7fd984
uzaircode <nikuzairsc@gmail.com>
2022-01-25 15:05:18 +0800

    third commit

--- src/com/lab3/question2.java
+++ src/com/lab3/question2.java
@@ -3,32 +3,45 @@ package com.lab3;
 import java.util.Scanner;
 
 public class question2 {
+
+
+    public static double getAreaRectangle(double w, double l) {
+        return (w*l);
+    }
+
+    public static double getRadiusCircle(double r) {
+        return (Math.PI * Math.pow(r, 2));
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double area;
+        double width, length;
 
         System.out.println("=======MENU=======");
         System.out.println("1. Calculate Area of Rectangle");
         System.out.println("2. Calculate Area of Circle");
         System.out.println("3. Exit Program");
 
-        System.out.print("Please enter your choice: ");
+        System.out.print("\nPlease enter your choice: ");
         int choice = input.nextInt();
 
         while (choice != 3) {
             switch (choice) {
                 case 1:
                     System.out.print("Enter width: ");
-                    double width = input.nextDouble();
+                    width = input.nextDouble();
                     System.out.print("Enter length: ");
-                    double length = input.nextDouble();
-                    area = width * length;
+                    length = input.nextDouble();
+
+                    area = getAreaRectangle(width,length);
                     break;
 
                 case 2:
                     System.out.print("Enter radius: ");
                     double radius = input.nextDouble();
-                    area = Math.PI * Math.pow(radius, 2);
+
+                    area = getRadiusCircle(radius);
                     break;
 
                 default:
